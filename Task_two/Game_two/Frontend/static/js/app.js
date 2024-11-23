d6846a82ad3d687a9d43d574fd3cd53c47696830
collins-hue <collinsmurgorqw@gmail.com>
2024-11-23 11:04:05 +0300

    Completed gama_two, the Memory Game

--- Task_two/Game_two/Frontend/static/js/app.js
+++ Task_two/Game_two/Frontend/static/js/app.js
@@ -1,54 +1,118 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
 
-let userChoice
-let computerChoice
-let result
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'static/images/fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'static/images/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'static/images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'static/images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'static/images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'static/images/pizza.png'
+    },
+    {
+        name: 'fries',
+        img: 'static/images/fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'static/images/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'static/images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'static/images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'static/images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'static/images/pizza.png'
+    },
+    
+]
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
-    generateComputerChoice ()
-    getResult()
-}))
+cardArray.sort(() => 0.5 - Math.random())
 
-function generateComputerChoice(){
-    const randomNumber = Math.floor(Math.random()*3 )+1 // or you could use possibleComputerChoices instead of 3.length
-    console.log(randomNumber)
+const gridDisplay = document.querySelector('#grid')
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
 
-    if (randomNumber == 1) {
-        computerChoice = 'rock'
-        console.log(computerChoice)
+
+function createBoard () {
+    for (let i = 0; i<cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'static/images/blank.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        gridDisplay.appendChild(card)
     }
-    if (randomNumber == 2) {
-        computerChoice = 'scissors'
-        console.log(computerChoice)
-    }
-    if (randomNumber == 3 ) {
-        computerChoice = 'paper'
-        console.log(computerChoice)
-    }
-    computerChoiceDisplay.innerHTML = computerChoice
 }
 
-function getResult() {
-    if(computerChoice  == userChoice){
-        result = 'It is a Draw'
+createBoard()
+
+function checkMatch() {
+    const cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
+    console.log(cards)
+    console.log('Check for Match!')
+    if (optionOneId == optionTwoId) {
+        cards[optionOneId].setAttribute('src', 'static/images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'static/images/blank.png')
+        alert('You clicked the same Image!')
     }
-    if(computerChoice == 'rock' && userChoice == 'paper') {
-        result = 'You Win!'
+    if (cardsChosen[0] == cardsChosen[1]) {
+        alert('You found a match!')
+        cards[optionOneId].setAttribute('src', 'static/images/white.png')
+        cards[optionTwoId].setAttribute('src', 'static/images/white.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', 'static/images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'static/images/blank.png')
+        alert('Sorry, try again!')
     }
-    if(computerChoice == 'rock' && userChoice == 'scissors') {
-        result = 'You Lost!'
+    resultDisplay.textContent = cardsWon.length
+    cardsChosen = []
+    cardsChosenIds = []
+
+    if (cardsWon.length == cardArray.length/2) {
+        resultDisplay.textContent = 'Congratulation, you won all!'
     }
-    if(computerChoice == 'paper' && userChoice == 'scissors') {
-        result = 'You Win!'
-    }
-    if(computerChoice == 'paper' && userChoice == 'scissors') {
-        result = 'You win!'
+}
+
+function flipCard() {
+    const cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    console.log(cardsChosen)
+    console.log(cardsChosenIds)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length == 2 ) {
+        setTimeout(checkMatch, 500)
     }
 
-    resultDisplay.innerHTML = result
 }
